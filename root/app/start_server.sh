096a70b6a2718281984d0771fd5f039abf55eb99
Pascal Paulis <ppaulis@gmail.com>
2026-09-02 23:11:04 +0200

    added possibility to disable web interface and changed 0/1 to true/false
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- root/app/start_server.sh
+++ root/app/start_server.sh
@@ -70,6 +70,98 @@ if grep -Fxq "MasterServerList=(Address=\"ut2004master2.epicgames.com\",Port=289
     sed -i '/MasterServerList=(Address="ut2004master2.epicgames.com",Port=28902)/c\MasterServerList=(Address="master.frag-net.com",Port=28902)' /data/config/UT2004.ini
 fi
 
+# If $SERVER_NAME is defined, set server name to this value
+if [ -n "${SERVER_NAME:-}" ]; then
+    sed -i "/\[Engine.GameReplicationInfo\]/,/^$/s/^ServerName=.*/ServerName=${SERVER_NAME}/" /data/config/UT2004.ini
+fi
+
+# Function to enable web interface
+enable_web_interface() {
+    if [ -z "${ADMIN_PASSWORD:-}" ]; then
+        echo "WARNING: ENABLE_WEB_INTERFACE is true but ADMIN_PASSWORD is not set - skipping web interface setup" >&2
+        return
+    fi
+    sed -i '/\[UWeb.WebServer\]/,/^$/s/bEnabled=.*/bEnabled=True/' /data/config/UT2004.ini
+    sed -i "/\[Engine.AccessControl\]/,/^$/s/^AdminPassword=.*/AdminPassword=${ADMIN_PASSWORD}/" /data/config/UT2004.ini
+}
+
+# Function to disable web interface
+disable_web_interface() {
+    sed -i '/\[UWeb.WebServer\]/,/^$/s/bEnabled=.*/bEnabled=False/' /data/config/UT2004.ini
+}
+
+# If $ENABLE_WEB_INTERFACE is defined, enable or disable the web interface accordingly
+if [ "${ENABLE_WEB_INTERFACE:-}" = "true" ]; then
+    enable_web_interface
+elif [ "${ENABLE_WEB_INTERFACE:-}" = "false" ]; then
+    disable_web_interface
+fi
+
+# xVoting configuration to add for Map Voting
+MAP_VOTING_CONFIG='
+[xVoting.xVotingHandler]
+VoteTimeLimit=30
+ScoreBoardDelay=10
+bAutoOpen=True
+MidGameVotePercent=50
+bScoreMode=False
+bAccumulationMode=False
+bEliminationMode=False
+MinMapCount=2
+MapVoteHistoryType=xVoting.MapVoteHistory_INI
+RepeatLimit=4
+DefaultGameConfig=0
+bDefaultToCurrentGameType=True
+bMapVote=True
+bKickVote=False
+bMatchSetup=False
+KickPercent=51
+bAnonymousKicking=True
+MapListLoaderType=xVoting.DefaultMapListLoader
+ServerNumber=1
+CurrentGameConfig=0
+GameConfig=(GameClass="XGame.xCTFGame",Prefix="CTF",Acronym="CTF",GameName="Capture The Flag",Mutators=,Options=)
+GameConfig=(GameClass="XGame.xCTFGame",Prefix="CTF",Acronym="CTF",GameName="Capture The Flag InstaGib",Mutators="XGame.MutInstaGib",Options=)
+GameConfig=(GameClass="Onslaught.ONSOnslaughtGame",Prefix="ONS",Acronym="ONS",GameName="Onslaught",Mutators=,Options=)
+GameConfig=(GameClass="XGame.xDeathMatch",Prefix="DM",Acronym="DM",GameName="DeathMatch",Mutators=,Options=)
+GameConfig=(GameClass="XGame.xDeathMatch",Prefix="DM",Acronym="DM",GameName="DeathMatch InstaGib",Mutators="XGame.MutInstaGib",Options=)
+GameConfig=(GameClass="XGame.xBombingRun",Prefix="BR",Acronym="BR",GameName="Bombing Run",Mutators=,Options=)
+GameConfig=(GameClass="UT2k4Assault.ASGameInfo",Prefix="AS",Acronym="AS",GameName="Assualt",Mutators=,Options=)
+GameConfig=(GameClass="XGame.xDoubleDom",Prefix="DOM",Acronym="DOM",GameName="Double Domination",Mutators=,Options=)
+GameConfig=(GameClass="XGame.xTeamGame",Prefix="DM",Acronym="DM",GameName="Team DeathMatch",Mutators=,Options=)
+
+[xVoting.DefaultMapListLoader]
+bUseMapList=False
+MapNamePrefixes=DM,DOM,CTF,BR,AS,ONS
+'
+
+# Function to enable map voting
+enable_map_voting() {
+    if ! grep -q "^\[xVoting\.xVotingHandler\]" "/data/config/UT2004.ini"; then
+        # Append map voting configuration if section doesn't exist
+        echo "$MAP_VOTING_CONFIG" >> "/data/config/UT2004.ini"
+    else
+        # Enable map voting if the section exists
+        sed -i '/\[xVoting.xVotingHandler\]/,/^$/s/^bMapVote=.*/bMapVote=True/' "/data/config/UT2004.ini"
+    fi
+}
+
+# Function to disable map voting
+disable_map_voting() {
+    if grep -q "^\[xVoting\.xVotingHandler\]" "/data/config/UT2004.ini"; then
+        # Section exists - just flip the flag off, leave the rest of the config intact
+        sed -i '/\[xVoting.xVotingHandler\]/,/^$/s/^bMapVote=.*/bMapVote=False/' "/data/config/UT2004.ini"
+    fi
+    # If the section doesn't exist at all, voting is already off by default - nothing to do
+}
+
+# If $ENABLE_MAP_VOTING is defined, enable or disable map voting accordingly
+if [ "${ENABLE_MAP_VOTING:-}" = "true" ]; then
+    enable_map_voting
+elif [ "${ENABLE_MAP_VOTING:-}" = "false" ]; then
+    disable_map_voting
+fi
+
 # If $CD_KEY is defined, update the server key
 if [ -n "${CD_KEY:-}" ]; then
     echo \"CDKey\"=\""${CD_KEY}"\" >/data/config/cdkey
